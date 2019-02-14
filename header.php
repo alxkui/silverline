6b89c86212b76911c1e4c561ccce068ba7d93a1b
Alex Kovacs <kovacs-a@ulster.ac.uk>
2019-02-14 15:04:24 +0000

    Fixed background issue

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     <link rel="stylesheet" href="./assets/style.css">
     <link rel="stylesheet" href="./assets/anim.css">
+    <link rel="stylesheet" href="./assets/breakpoints.css" media="screen">
+    <link rel="stylesheet" href="./assets/print.css" media="print">
     <title>Silverline Inc. Home</title>
 </head>
 <body>
