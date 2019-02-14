6575dbe480d1891cce23f731e16b92cf8f2837dc
Alex Kovacs <kovacs-a@ulster.ac.uk>
2019-02-14 15:34:49 +0000

    Footer changed for larger screens (grid)

--- footer.php
+++ footer.php
@@ -1,36 +1,38 @@
     <footer class="content" id="main-footer">
         <div class="container">
         
-        <div class="foot-item">
-            <h3>Follow Us</h3>
-            <div class="social">
-                <a href="#" class="fab fa-facebook"></a>
-                <a href="#" class="fab fa-instagram"></a>
-                <a href="#" class="fab fa-twitter"></a>
-            </div>
-        </div>
-
-        <div class="foot-item">
-            <h3>Contact info</h3>
-            <div class="title">
-                <span class="brand fas fa-envelope"></span>
-                <span>email@silverline.com</span>
+        <div class="grid-3">
+            <div class="foot-item">
+                <h3>Follow Us</h3>
+                <div class="social">
+                    <a href="#" class="fab fa-facebook"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                </div>
             </div>
 
-            <div class="title">
-                <span class="brand fas fa-phone"></span>
-                <span>02837482123</span>
+            <div class="foot-item">
+                <h3>Contact info</h3>
+                <div class="title">
+                    <span class="brand fas fa-envelope"></span>
+                    <span>email@silverline.com</span>
+                </div>
+
+                <div class="title">
+                    <span class="brand fas fa-phone"></span>
+                    <span>02837482123</span>
+                </div>
+
+                <div class="title">
+                    <span class="brand fas fa-building"></span>
+                    <span>Some address</span>
+                </div>
             </div>
 
-            <div class="title">
-                <span class="brand fas fa-building"></span>
-                <span>Some address</span>
+            <div class="foot-item">
+                <h3>Other info</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris metus.</p>
             </div>
-        </div>
-
-        <div class="foot-item">
-            <h3>Other info</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris metus.</p>
         </div>
 
         <br />
